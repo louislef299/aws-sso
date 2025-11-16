a9832ec07258ac71193df0e73aa146a82ddee52f
Louis LeFebvre <louislefebvre1999@gmail.com>
2025-11-16 13:12:43 -0600

    docs: Add comments for clarity

--- pkg/os/signals_unix.go
+++ pkg/os/signals_unix.go
@@ -8,8 +8,15 @@ import (
 )
 
 var Signals = []os.Signal{
+	// Interrupt from keyboard
 	os.Interrupt,
+
+	// Hangup detected on controlling terminal or death of controlling process
 	syscall.SIGHUP,
-	syscall.SIGTERM,
+
+	// Quit from keyboard
 	syscall.SIGQUIT,
+
+	// Termination signal
+	syscall.SIGTERM,
 }
